d66e2c5fccf77715bbc046fdf58e7c5826068a63
Igor Ignatov <kantorawb@gmail.com>
2023-09-14 18:29:21 +0200

    fix: run requests in main thread

--- FakeNFT/Services/Requests/NFTCartManager.swift
+++ FakeNFT/Services/Requests/NFTCartManager.swift
@@ -9,14 +9,14 @@ struct NFTCartManager {
             case .success(let models):
                 let dispatchGroup = DispatchGroup()
                 var nfts: [NFTServerModel] = []
-
+                
                 for id in models.nfts {
                     dispatchGroup.enter()
                     fetchNFTs(id: id) { nftResult in
                         defer {
                             dispatchGroup.leave()
                         }
-
+                        
                         switch nftResult {
                         case .success(let nft):
                             nfts.append(nft)
@@ -26,7 +26,7 @@ struct NFTCartManager {
                         }
                     }
                 }
-
+                
                 dispatchGroup.notify(queue: .main) {
                     completion(.success(nfts))
                 }
@@ -36,15 +36,18 @@ struct NFTCartManager {
             }
         }
     }
-
+    
     private func fetchCart(completion: @escaping (Result<Order, Error>) -> Void) {
         let request = NFTNetworkRequest(
             endpoint: URL(string: Constants.ordersAPI.rawValue)!,
             httpMethod: .get,
             dto: nil)
-        networkClient.send(request: request,
-                           type: Order.self,
-                           onResponse: completion)
+        
+        networkClient.send(request: request, type: Order.self) { result in
+            DispatchQueue.main.async {
+                completion(result)
+            }
+        }
     }
     
     private func fetchNFTs(id: String, completion: @escaping (Result<NFTServerModel, Error>) -> Void) {
@@ -52,9 +55,12 @@ struct NFTCartManager {
             endpoint: URL(string: Constants.nftAPI.rawValue + "\(id)")!,
             httpMethod: .get,
             dto: nil)
-        networkClient.send(request: request,
-                           type: NFTServerModel.self,
-                           onResponse: completion)
+        
+        networkClient.send(request: request, type: NFTServerModel.self) { result in
+            DispatchQueue.main.async {
+                completion(result)
+            }
+        }
     }
     
     func removeNFTFromCart(id: String, nfts: [String], completion: @escaping (Result<Order, Error>) -> Void) {
@@ -62,7 +68,12 @@ struct NFTCartManager {
             endpoint: URL(string: Constants.ordersAPI.rawValue)!,
             httpMethod: .put,
             dto: Order(id: id, nfts: nfts))
-        networkClient.send(request: request, type: Order.self, onResponse: completion)
+        
+        networkClient.send(request: request, type: Order.self) { result in
+            DispatchQueue.main.async {
+                completion(result)
+            }
+        }
     }
     
     func addNFTFromStatistics(id: String, nfts: [String], completion: @escaping (Result<Order, Error>) -> Void) {
@@ -70,6 +81,11 @@ struct NFTCartManager {
             endpoint: URL(string: Constants.ordersAPI.rawValue),
             httpMethod: .put,
             dto: Order(id: id, nfts: nfts))
-        networkClient.send(request: request, type: Order.self, onResponse: completion)
+        
+        networkClient.send(request: request, type: Order.self) { result in
+            DispatchQueue.main.async {
+                completion(result)
+            }
+        }
     }
 }
